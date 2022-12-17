bffdb9d5c0bf740a191c4094e368b97a6fba332e
cetteup <me@cetteup.com>
2022-12-17 19:05:11 +0100

    chore: Update credits

--- pyut2serverlist/__init__.py
+++ pyut2serverlist/__init__.py
@@ -11,5 +11,9 @@ Simple Python library for querying Unreal Engine 2 based principal servers and t
 
 __version__ = '0.0.2'
 __author__ = 'cetteup'
-__credits__ = 'https://github.com/CVSoft/MSQuery'
+__credits__ = [
+    'https://github.com/CVSoft/MSQuery',
+    'https://github.com/Austinb/GameQ',
+    'https://github.com/gamedig/node-gamedig'
+]
 __all__ = ['Connection', 'PrincipalServer', 'Server', 'Error', 'ConnectionError', 'TimeoutError', 'AuthError']
